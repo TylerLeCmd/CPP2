0350899035f9c80b318733ec8ffed72b422746a1
YunaSan <70344578+TylerLeCmd@users.noreply.github.com>
2023-09-06 06:42:25 -0600

    Update Cuboid.java

--- src/ShapeTesting/Cuboid.java
+++ src/ShapeTesting/Cuboid.java
@@ -2,15 +2,18 @@ public class Cuboid {
     private float l;
     private float w;
     private float h;
-    public void setVals(float length, float width, float height){
+
+    public void setVals(float length, float width, float height) {
         this.l = length;
         this.w = width;
         this.h = height;
     };
-    public float getVolume (){
-        return (l*w*h);
+
+    public float getVolume() {
+        return (l * w * h);
     }
-    public float getSA (){
-        return ((2*l*w)+(2*w*h)+(2*l*h));
+
+    public float getSA() {
+        return ((2 * l * w) + (2 * w * h) + (2 * l * h));
     }
 }
