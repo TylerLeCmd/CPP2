0a766d14d129b2a3ec611b01e288fac441f9472c
YunaSan <70344578+TylerLeCmd@users.noreply.github.com>
2023-09-19 21:44:22 -0600

    Update Obsurdle.java

--- src/Obsurdle/Obsurdle.java
+++ src/Obsurdle/Obsurdle.java
@@ -18,49 +18,52 @@ public class Obsurdle {
         char[] cLetters = chosenWord.toCharArray();
 
         // Output Logic
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        System.out.println(" -- -- -- -- -- -- -- ");
+        System.out.println("| O| B| S| U| R| D| L|");
+        System.out.println(" -- -- -- -- -- -- -- ");
+        System.out.println("| E| O| B| S| U| R| D|");
+        System.out.println(" -- -- -- -- -- -- -- ");
+        System.out.println("| L| E| O| B| S| U| R|");
+        System.out.println(" -- -- -- -- -- -- -- ");
+        System.out.println("| D| L| E| O| B| S| U|");
+        System.out.println(" -- -- -- -- -- -- -- ");
+        System.out.println("| R| D| L| E| O| B| S|");
+        System.out.println(" -- -- -- -- -- -- -- ");
+        System.out.println("| U| R| D| L| E| O| B|");
+        System.out.println(" -- -- -- -- -- -- -- ");
+        System.out.println("| S| U| R| D| L| E| O|");
+        System.out.println(" -- -- -- -- -- -- -- ");
+        System.out.println("                ");
+        System.out.println("                ");
+        System.out.println("Welcome to OBSURDLE.");
         while (runtime) {
-            System.out.print("\033[H\033[2J");
-            System.out.flush();
-
-            System.out.println(" -- -- -- -- -- -- -- ");
-            System.out.println("| O| B| S| U| R| D| L|");
-            System.out.println(" -- -- -- -- -- -- -- ");
-            System.out.println("| E| O| B| S| U| R| D|");
-            System.out.println(" -- -- -- -- -- -- -- ");
-            System.out.println("| L| E| O| B| S| U| R|");
-            System.out.println(" -- -- -- -- -- -- -- ");
-            System.out.println("| D| L| E| O| B| S| U|");
-            System.out.println(" -- -- -- -- -- -- -- ");
-            System.out.println("| R| D| L| E| O| B| S|");
-            System.out.println(" -- -- -- -- -- -- -- ");
-            System.out.println("| U| R| D| L| E| O| B|");
-            System.out.println(" -- -- -- -- -- -- -- ");
-            System.out.println("| S| U| R| D| L| E| O|");
-            System.out.println(" -- -- -- -- -- -- -- ");
-            System.out.println("                ");
-            System.out.println("                ");
-            System.out.println("Welcome to OBSURDLE.");
-            if (!lengthMatch){
+            if (!lengthMatch) {
                 System.out.println("Your word isn't seven letters long! Please try again!");
+                lengthMatch = true;
             }
             System.out.println("To begin, guess a 7-letter word.");
             String guess = beans.nextLine();
 
             // Word Validity
             char[] gLetters = guess.toCharArray();
-            try{
-                char sev = gLetters[6];
-                char eig = gLetters [7];
-            }catch(Exception e){
-
+            try {
+                try {
+                    char eig = gLetters[7];
+                } catch (Exception e) {
+                    char sev = gLetters[6];
+                }
+            } catch (Exception e) {
+                lengthMatch = false;
             }
+
             // Game Logic
-            // System.out.println(chosenWord);
-            // System.out.println("beans");
-            // for (int i = 0; i < 7; i++) {
-            //     System.out.println(cLetters[i] + " " + gLetters[i]);
-            // }
-            // beans.close();
+            System.out.println(chosenWord);
+            for (int i = 0; i < 7; i++) {
+            System.out.println(cLetters[i] + " " + gLetters[i]);
+            }
+            beans.close();
         }
     }
 }
